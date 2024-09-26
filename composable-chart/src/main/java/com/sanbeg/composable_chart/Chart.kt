59f7df573c54df3d519cd77278f368035ad8b649
Steve Sanbeg <steve.sanbeg@gmail.com>
2024-09-25 22:12:08 -0400

    test data inset as modifier, not really useful

--- composable-chart/src/main/java/com/sanbeg/composable_chart/Chart.kt
+++ composable-chart/src/main/java/com/sanbeg/composable_chart/Chart.kt
@@ -1,6 +1,7 @@
 package com.sanbeg.composable_chart
 
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -16,7 +17,14 @@ import androidx.compose.ui.unit.dp
 class ComposableChartScope internal constructor(
     internal val minX: Float,
     internal val maxX: Float,
-)
+    ) {
+    internal var dataInset: Float = 0f
+        private set
+    fun Modifier.dataInset(inset: Float): Modifier  {
+        dataInset = inset
+        return this
+    }
+}
 
 class ComposableChartScaleScope internal constructor(
     internal val matrix: Matrix,
@@ -42,9 +50,11 @@ fun ComposableChartScope.Scale(
     ) {
     Spacer(modifier.drawBehind {
         Matrix().apply {
+            translate(x = dataInset, y = dataInset)
+            val di2 = dataInset * 2
             scale(
-                x = size.width / (maxX - minX),
-                y = size.height / -(maxY - minY),
+                x = (size.width - di2) / (maxX - minX),
+                y = (size.height - di2) / -(maxY - minY),
             )
             translate(
                 y = -maxY
@@ -71,10 +81,14 @@ fun ComposableChartScaleScope.drawEach(
 @Composable
 fun previewChart() {
     Chart(maxX = 100f) {
-        Scale(maxY = 100f) {
+        Scale(maxY = 100f, modifier = Modifier.size(100.dp).dataInset(10f)) {
             // drawScope.drawCircle(Color.Red, 4.dp.value)
             drawEach(
-                listOf(Offset(25f, 25f))
+                listOf(
+                    Offset(25f, 25f),
+                    Offset(0f, 0f),
+                    Offset(100f, 100f),
+                )
             ) {
                 drawCircle(Color.Blue, 4.dp.value, it)
             }
