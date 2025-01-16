6063d3eae8027cbecab573a350a959c1509ac091
Steve Sanbeg <steve.sanbeg@gmail.com>
2025-01-15 19:21:57 -0500

    rm old collections

--- composable-chart/src/main/java/com/sanbeg/composable_chart/collections/OffsetCollection.kt
+++
@@ -1,9 +0,0 @@
-package com.sanbeg.composable_chart.collections
-
-import androidx.compose.runtime.Stable
-import androidx.compose.ui.geometry.Offset
-
-@Stable
-interface OffsetCollection: Collection<Offset> {
-    // fun map(transform: (Offset) -> Offset): OffsetCollection
-}
